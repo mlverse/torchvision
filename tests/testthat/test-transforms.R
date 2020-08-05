0f8e4106a50baf51659d95ea2cf74dd07bb6f769
Daniel Falbel <dfalbel@gmail.com>
2020-08-05 16:28:26 -0300

    make smaller img

--- tests/testthat/test-transforms.R
+++ tests/testthat/test-transforms.R
@@ -1,6 +1,6 @@
 test_that("convert_image_dtype", {
 
-  x <- torch::torch_rand(3, 10, 10, dtype = torch_float32())
+  x <- torch::torch_rand(1, 2, 2, dtype = torch_float32())
   o <- transform_convert_image_dtype(x, torch_int16())
   y <- transform_convert_image_dtype(o, torch_float32())
 
