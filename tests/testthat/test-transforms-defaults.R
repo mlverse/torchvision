05992c05232929c2ee2e91aee80dd6ed6bcf2bcc
Daniel Falbel <dfalbel@gmail.com>
2020-09-21 16:04:49 -0300

    fix path for imgs

--- tests/testthat/test-transforms-defaults.R
+++ tests/testthat/test-transforms-defaults.R
@@ -4,7 +4,7 @@ test_that("random_resised_crop", {
   o <- transform_random_resized_crop(img, size = c(32, 32))
   expect_tensor_shape(o, c(3, 32,32))
 
-  im <- magick::image_read("tests/testthat/torch.png")
+  im <- magick::image_read("torch.png")
   o <- transform_random_resized_crop(im, size = c(32, 32))
   expect_tensor_shape(transform_to_tensor(o), c(3, 32, 32))
 
--- tests/testthat/test-transforms-magick.R
+++ tests/testthat/test-transforms-magick.R
@@ -1,5 +1,5 @@
 test_that("multiplication works", {
-  im <- magick::image_read("tests/testthat/torch.png")
+  im <- magick::image_read("torch.png")
   transform_crop(im, 1, 1, 500, 500)
 
   im <- transform_random_resized_crop(im, size = c(224, 224))
