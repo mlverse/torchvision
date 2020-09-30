35717fa199a29aa09f78d26a8ab64e07c2976308
Daniel Falbel <dfalbel@gmail.com>
2020-09-30 20:28:55 -0300

    ass test and fix #12

---
+++ tests/testthat/test-folder-dataset.R
@@ -0,0 +1,16 @@
+test_that("image_folder dataset", {
+
+  ds <- image_folder_dataset(
+    root = "assets/class",
+    transform = . %>% transform_to_tensor %>%
+      transform_resize(c(32,32))
+  )
+  expect_length(ds[1], 2)
+
+  dl <- torch::dataloader(ds, batch_size = 2, drop_last = TRUE)
+  for(batch in torch::enumerate(dl)) {
+    expect_tensor_shape(batch[[1]], c(2, 3, 32, 32))
+    expect_tensor_shape(batch[[2]], 2)
+  }
+
+})
