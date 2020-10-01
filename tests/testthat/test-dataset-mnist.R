78f51e606f41528a835725afa8668c56a356b4a9
Daniel Falbel <dfalbel@gmail.com>
2020-10-01 17:51:10 -0300

    modify mnist datasets

--- tests/testthat/test-dataset-mnist.R
+++ tests/testthat/test-dataset-mnist.R
@@ -2,36 +2,32 @@ context("dataset-mnist")
 
 test_that("tests for the mnist dataset", {
 
-  # skipped on mac because I use mac and I don't want too download mnist evrytime
-  #skip_on_os("mac")
-
   dir <- tempfile(fileext = "/")
 
   expect_error(
     ds <- mnist_dataset(dir)
   )
 
-  ds <- mnist_dataset(dir, download = TRUE, transform = function(x) {
-    torch::torch_tensor(x)
-  })
+  ds <- mnist_dataset(dir, download = TRUE)
 
   i <- ds[1]
-
-  expect_tensor(i[[1]])
-  expect_tensor(i[[2]]$to(dtype = torch_int()))
-
-  expect_tensor_shape(torch::torch_tensor(ds$data), c(60000, 28, 28))
-  expect_tensor_shape(torch::torch_tensor(ds$targets)$to(dtype = torch_int()), c(60000))
-
+  expect_equal(dim(i[[1]]), c(28, 28))
+  expect_equal(i[[2]], 6)
   expect_equal(length(ds), 60000)
+
+  ds <- mnist_dataset(dir, transform = transform_to_tensor)
+  dl <- torch::dataloader(ds, batch_size = 32)
+  expect_length(dl, 1875)
+  iter <- dataloader_make_iter(dl)
+  i <- dataloader_next(iter)
+  expect_tensor_shape(i[[1]], c(32, 1, 28, 28))
+  expect_tensor_shape(i[[2]], 32)
+  expect_true((torch_max(i[[1]]) <= 1)$item())
 
 })
 
 
 test_that("tests for the kmnist dataset", {
-
-  # skipped on mac because I use mac and I don't want too download mnist evrytime
-  skip_on_os("mac")
 
   dir <- tempfile(fileext = "/")
 
@@ -39,17 +35,19 @@ test_that("tests for the kmnist dataset", {
     ds <- kmnist_dataset(dir)
   )
 
-  ds <- kmnist_dataset(dir, download = TRUE, transform = function(x) {
-    torch::torch_tensor(x)
-  })
+  ds <- kmnist_dataset(dir, download = TRUE)
+
   i <- ds[1]
-
-  expect_tensor(i[[1]])
-  expect_tensor(i[[2]])
-  expect_equal(length(i[[2]]$shape), 0)
-
-  expect_tensor_shape(torch::torch_tensor(ds$data), c(60000, 28, 28))
-  expect_tensor_shape(torch::torch_tensor(ds$targets)$to(dtype = torch_int()), c(60000))
-
+  expect_equal(dim(i[[1]]), c(28, 28))
+  expect_equal(i[[2]], 9)
   expect_equal(length(ds), 60000)
+
+  ds <- kmnist_dataset(dir, transform = transform_to_tensor)
+  dl <- torch::dataloader(ds, batch_size = 32)
+  expect_length(dl, 1875)
+  iter <- dataloader_make_iter(dl)
+  i <- dataloader_next(iter)
+  expect_tensor_shape(i[[1]], c(32, 1, 28, 28))
+  expect_tensor_shape(i[[2]], 32)
+  expect_true((torch_max(i[[1]]) <= 1)$item())
 })
