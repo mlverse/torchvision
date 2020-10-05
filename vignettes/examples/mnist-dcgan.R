cf3524489060e9cc34e2e429c0f3657bb2cf3fa2
Daniel Falbel <dfalbel@gmail.com>
2020-10-05 16:39:47 -0300

    reorganize dcgan example

--- vignettes/examples/mnist-dcgan.R
+++ vignettes/examples/mnist-dcgan.R
@@ -1,35 +1,54 @@
+# Packages ----------------------------------------------------------------
+
 library(torch)
+library(torchvision)
+
+# Utils -------------------------------------------------------------------
+
+# plots an image generated given the
+# intermediate state
+plot_gen <- function(noise) {
+  img <- G(noise)
+  img <- img$cpu()
+  img <- img[1,1,,,newaxis]/2 + 0.5
+  img <- torch_stack(list(img, img, img), dim = 3)[..,1]
+  img <- as.raster(as_array(img))
+  plot(img)
+}
+
+
+# Datasets and loaders ----------------------------------------------------
 
 dir <- "~/Downloads/mnist"
 
 ds <- mnist_dataset(
-  dir, 
-  download = TRUE, 
-  transform = function(x) {
-    x <- x$to(dtype = torch_float())/256
-    x <- 2*(x - 0.5)
-    x[newaxis,..]
-  }
+  dir,
+  transform = transform_to_tensor,
+  download = TRUE,
 )
+
 dl <- dataloader(ds, batch_size = 32, shuffle = TRUE)
 
+
+# Define the network ------------------------------------------------------
+
 generator <- nn_module(
-  "generator", 
+  "generator",
   initialize = function(latent_dim, out_channels) {
     self$main <- nn_sequential(
-      nn_conv_transpose2d(latent_dim, 512, kernel_size = 4, 
+      nn_conv_transpose2d(latent_dim, 512, kernel_size = 4,
                           stride = 1, padding = 0, bias = FALSE),
       nn_batch_norm2d(512),
       nn_relu(),
-      nn_conv_transpose2d(512, 256, kernel_size = 4, 
+      nn_conv_transpose2d(512, 256, kernel_size = 4,
                           stride = 2, padding = 1, bias = FALSE),
       nn_batch_norm2d(256),
       nn_relu(),
-      nn_conv_transpose2d(256, 128, kernel_size = 4, 
+      nn_conv_transpose2d(256, 128, kernel_size = 4,
                           stride = 2, padding = 1, bias = FALSE),
       nn_batch_norm2d(128),
       nn_relu(),
-      nn_conv_transpose2d(128, out_channels, kernel_size = 4, 
+      nn_conv_transpose2d(128, out_channels, kernel_size = 4,
                           stride = 2, padding = 3, bias = FALSE),
       nn_tanh()
     )
@@ -65,15 +84,6 @@ discriminator <- nn_module(
   }
 )
 
-plot_gen <- function(noise) {
-  img <- G(noise)
-  img <- img$cpu()
-  img <- img[1,1,,,newaxis]/2 + 0.5
-  img <- torch_stack(list(img, img, img), dim = 2)[..,1]
-  img <- as.raster(as_array(img))
-  plot(img)
-}
-
 device <- torch_device(ifelse(cuda_is_available(),  "cuda", "cpu"))
 
 G <- generator(latent_dim = 100, out_channels = 1)
@@ -85,7 +95,7 @@ init_weights <- function(m) {
   } else if (grepl("batch_norm", m$.classes[[1]])) {
     nn_init_normal_(m$weight$data(), 1.0, 0.02)
     nn_init_constant_(m$bias$data(), 0)
-  } 
+  }
 }
 
 G[[1]]$apply(init_weights)
@@ -99,47 +109,50 @@ D_optimizer <- optim_adam(D$parameters, lr = 2 * 1e-4, betas = c(0.5, 0.999))
 
 fixed_noise <- torch_randn(1, 100, 1, 1, device = device)
 
+
+# Training loop -----------------------------------------------------------
+
 loss <- nn_bce_loss()
 
 for (epoch in 1:10) {
-  
+
   pb <- progress::progress_bar$new(
-    total = length(dl), 
+    total = length(dl),
     format = "[:bar] :eta Loss D: :lossd Loss G: :lossg"
   )
   lossg <- c()
   lossd <- c()
-  
+
   for (b in enumerate(dl)) {
-    
+
     y_real <- torch_ones(32, device = device)
     y_fake <- torch_zeros(32, device = device)
-    
+
     noise <- torch_randn(32, 100, 1, 1, device = device)
     fake <- G(noise)
-    
+
     img <- b[[1]]$to(device = device)
-    
+
     # train the discriminator ---
     D_loss <- loss(D(img), y_real) + loss(D(fake$detach()), y_fake)
-    
+
     D_optimizer$zero_grad()
     D_loss$backward()
     D_optimizer$step()
-    
+
     # train the generator ---
-    
+
     G_loss <- loss(D(fake), y_real)
-    
+
     G_optimizer$zero_grad()
     G_loss$backward()
     G_optimizer$step()
-    
+
     lossd <- c(lossd, D_loss$item())
     lossg <- c(lossg, G_loss$item())
     pb$tick(tokens = list(lossd = mean(lossd), lossg = mean(lossg)))
   }
+
   plot_gen(fixed_noise)
-  
   cat(sprintf("Epoch %d - Loss D: %3f Loss G: %3f\n", epoch, mean(lossd), mean(lossg)))
 }
