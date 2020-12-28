edac2641d7c4126151e1317887796cbdbf633f24
Daniel Falbel <dfalbel@gmail.com>
2020-12-28 13:16:10 -0300

    add vgg models

---
+++ tests/testthat/test-models-vgg.R
@@ -0,0 +1,28 @@
+test_that("vgg models works", {
+
+  vggs <- list(
+    model_vgg11,
+    model_vgg11_bn,
+    model_vgg13,
+    model_vgg13_bn,
+    model_vgg16,
+    model_vgg16_bn,
+    model_vgg19,
+    model_vgg19_bn
+  )
+
+  for (m in vggs) {
+
+    model <- m()
+    expect_tensor_shape(model(torch_ones(5, 3, 224, 224)), c(5, 1000))
+
+  }
+
+  skip_on_os(os = "mac") # not downloading a bunch of files locally.
+
+  for (m in vggs) {
+    model <- m(pretrained = TRUE)
+    expect_tensor_shape(model(torch_ones(5, 3, 224, 224)), c(5, 1000))
+  }
+
+})
