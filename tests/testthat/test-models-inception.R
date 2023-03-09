11c4711033eed1242dc69d3cd2e61ea74f0366aa
Daniel Falbel <dfalbel@gmail.com>
2023-03-09 10:37:34 +0100

    Add imagenet class mapping.

--- tests/testthat/test-models-inception.R
+++ tests/testthat/test-models-inception.R
@@ -6,3 +6,32 @@ test_that("inception_v3 pretrained", {
   expect_equal(as.numeric(x[1,1]), 0.18005196750164032, tol = 5e-6)
   expect_tensor_shape(x, c(2, 1000))
 })
+
+test_that("produces correctly classification", {
+  model <- model_inception_v3(pretrained = TRUE)
+  model$eval()
+
+  ds <- torchvision::image_folder_dataset(
+    testthat::test_path("assets/class"),
+    transform = function(x) {
+      x <- transform_to_tensor(x)
+      x <- transform_resize(x, size = c(299, 299))
+      x <- torchvision::transform_normalize(
+        x,
+        mean = c(0.485, 0.456, 0.406),
+        std = c(0.229, 0.224, 0.225)
+      )
+      x
+    })
+  dl <- dataloader(ds, batch_size = 5)
+  batch <- dataloader_next(dataloader_make_iter(dl))
+
+  pred <- model(batch[[1]])
+  pred <- pred$argmax(dim=-1)
+  expect_equal(
+    imagenet_classes()[as.integer(pred)],
+    c("Pembroke, Pembroke Welsh corgi", "tabby, tabby cat", "tabby, tabby cat",
+      "Egyptian cat", "Egyptian cat")
+  )
+
+})
