ffb8f6a31f260fbfaf514a4154d8161fb91530e3
C. Regouby <christophe.regouby@free.fr>
2024-08-21 21:15:19 +0200

    fix test

--- tests/testthat/test-message-translations.R
+++ tests/testthat/test-message-translations.R
@@ -10,7 +10,7 @@ test_that("R-level type_error messages are correctly translated in FR", {
 
 test_that("R-level value_error messages are correctly translated in FR", {
   withr::with_language(lang = "fr",
-                       expect_warning(
+                       expect_error(
                          transform_adjust_gamma(torch::torch_rand(c(4, 4, 3)), gamma = -0.5),
                         regexp = "`gamma` doit être positif",
                         fixed = TRUE
