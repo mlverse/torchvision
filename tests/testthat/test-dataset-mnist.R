49d8ba4d2d292e58fe2ef11a55158fc2ae0d3fb9
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-04 02:09:52 +0530

    Addition of tests for QMNIST Dataset #131

--- tests/testthat/test-dataset-mnist.R
+++ tests/testthat/test-dataset-mnist.R
@@ -53,3 +53,30 @@ test_that("tests for the kmnist dataset", {
   expect_true((torch_max(i[[1]]) <= 1)$item())
   expect_named(i, c("x", "y"))
 })
+
+test_that("tests for the qmnist dataset", {
+
+  dir <- tempfile(fileext = "/")
+
+  expect_error(
+    ds <- qmnist_dataset(dir)
+  )
+
+  ds <- qmnist_dataset(dir, download = TRUE)
+
+  i <- ds[1]
+  expect_equal(dim(i[[1]]), c(28, 28))
+  expect_true(i[[2]] %in% 1:10)
+  expect_equal(length(ds), 60000)
+
+  ds <- qmnist_dataset(dir, transform = transform_to_tensor)
+  dl <- torch::dataloader(ds, batch_size = 32)
+  expect_length(dl, 1875)
+  iter <- dataloader_make_iter(dl)
+  i <- dataloader_next(iter)
+  expect_tensor_shape(i[[1]], c(32, 1, 28, 28))
+  expect_tensor_shape(i[[2]], 32)
+  expect_true((torch_max(i[[1]]) <= 1)$item())
+  expect_named(i, c("x", "y"))
+
+})
