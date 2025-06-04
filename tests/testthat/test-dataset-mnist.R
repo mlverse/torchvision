ff2d0f61c6656b675ffb627ebc58cbf07641345c
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-04 16:55:37 +0530

    Switching the parameter train=TRUE into split = c(byclass, bymerge, balanced, letters, digits, mnist)

--- tests/testthat/test-dataset-mnist.R
+++ tests/testthat/test-dataset-mnist.R
@@ -54,30 +54,44 @@ test_that("tests for the kmnist dataset", {
   expect_named(i, c("x", "y"))
 })
 
-test_that("tests for the emnist (balanced split) dataset", {
+test_that("tests for the emnist dataset", {
 
   dir <- tempfile(fileext = "/")
 
-  expect_error(
-    ds <- emnist_dataset(dir)
+  splits <- list(
+    byclass  = list(n_classes = 62, n_train = 697932),
+    bymerge  = list(n_classes = 47, n_train = 697932),
+    balanced = list(n_classes = 47, n_train = 112800),
+    letters  = list(n_classes = 26, n_train = 124800),
+    digits   = list(n_classes = 10, n_train = 240000),
+    mnist    = list(n_classes = 10, n_train = 60000)
   )
 
-  ds <- emnist_dataset(dir, download = TRUE)
+  for (split in names(splits)) {
+    info <- splits[[split]]
+    #cat("Testing split:", split, "\n")
 
-  i <- ds[1]
-  expect_equal(dim(i[[1]]), c(28, 28))
-  expect_true(i[[2]] %in% 0:46)  # Label should be within expected range
-  expect_equal(length(ds), 112800)
+    expect_error(
+      ds <- emnist_dataset(dir, split = split)
+    )
 
-  ds <- emnist_dataset(dir, transform = transform_to_tensor)
-  dl <- torch::dataloader(ds, batch_size = 32)
-  expect_length(dl, ceiling(112800 / 32))
-  
-  iter <- dataloader_make_iter(dl)
-  batch <- dataloader_next(iter)
-  
-  expect_tensor_shape(batch[[1]], c(32, 1, 28, 28))
-  expect_tensor_shape(batch[[2]], 32)
-  expect_true((torch_max(batch[[1]]) <= 1)$item())
-  expect_named(batch, c("x", "y"))
+    ds <- emnist_dataset(dir, split = split, download = TRUE)
+    
+    i <- ds[1]
+    expect_equal(dim(i[[1]]), c(28, 28))
+    expect_true(i[[2]] %in% 0:(info$n_classes - 1))
+    expect_equal(length(ds), info$n_train)
+
+    ds <- emnist_dataset(dir, split = split, transform = transform_to_tensor)
+    dl <- dataloader(ds, batch_size = 32)
+    expect_length(dl, ceiling(info$n_train / 32))
+
+    iter <- dataloader_make_iter(dl)
+    batch <- dataloader_next(iter)
+
+    expect_tensor_shape(batch[[1]], c(32, 1, 28, 28))
+    expect_tensor_shape(batch[[2]], 32)
+    expect_true((torch_max(batch[[1]]) <= 1)$item())
+    expect_named(batch, c("x", "y"))
+  }
 })
