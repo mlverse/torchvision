f0e5a20eefe04aa22d2791c5dd71a881e0c5da17
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-08 21:56:55 +0530

    Addition of test for Flickr30k Dataset #135

--- tests/testthat/test-dataset-flickr.R
+++ tests/testthat/test-dataset-flickr.R
@@ -3,10 +3,6 @@ context("dataset-flickr")
 test_that("tests for the flickr8k dataset", {
   t <- tempfile()
 
-  expect_error(
-    flickr8k_dataset(root = t, download = FALSE)
-  )
-
   flickr8k <- flickr8k_dataset(root = t, train = TRUE, download = TRUE)
   expect_equal(length(flickr8k), 6000)
   first_item <- flickr8k[1]
@@ -28,5 +24,32 @@ test_that("tests for the flickr8k dataset", {
   expect_equal((first_item$y[[3]]), "Two brown dogs playfully fight in the snow .")
   expect_equal((first_item$y[[4]]), "Two brown dogs wrestle in the snow .")
   expect_equal((first_item$y[[5]]), "Two dogs playing in the snow .")
+
+})
+
+test_that("tests for the flickr30k dataset", {
+  t <- tempfile()
+
+  flickr30k <- flickr30k_dataset(root = t, train = TRUE, download = TRUE)
+  expect_equal(length(flickr30k), 29000)
+  first_item <- flickr30k[1]
+  expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "torch_tensor"))
+  expect_equal((first_item$y[[1]]), "Two young guys with shaggy hair look at their hands while hanging out in the yard.")
+  expect_equal((first_item$y[[2]]), "Two young, White males are outside near many bushes.")
+  expect_equal((first_item$y[[3]]), "Two men in green shirts are standing in a yard.")
+  expect_equal((first_item$y[[4]]), "A man in a blue shirt standing in a garden.")
+  expect_equal((first_item$y[[5]]), "Two friends enjoy time spent together.")
+
+  flickr30k <- flickr30k_dataset(root = t, train = FALSE, download = TRUE)
+  expect_equal(length(flickr30k), 1000)
+  first_item <- flickr30k[1]
+  expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "torch_tensor"))
+  expect_equal((first_item$y[[1]]), "The man with pierced ears is wearing glasses and an orange hat.")
+  expect_equal((first_item$y[[2]]), "A man with glasses is wearing a beer can crocheted hat.")
+  expect_equal((first_item$y[[3]]), "A man with gauges and glasses is wearing a Blitz hat.")
+  expect_equal((first_item$y[[4]]), "A man in an orange hat starring at something.")
+  expect_equal((first_item$y[[5]]), "A man wears an orange hat and glasses.")
 
 })
