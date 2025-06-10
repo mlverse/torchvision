528bd4a3a9a9c85f5e299c7e8682ceafbc6ecc28
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-10 21:45:31 +0530

    Reductionv2.0 tests for EMNIST Dataset #129

--- tests/testthat/test-dataset-mnist.R
+++ tests/testthat/test-dataset-mnist.R
@@ -76,47 +76,39 @@ test_that("fashion_mnist_dataset loads correctly", {
   expect_tensor_shape(batch$y, 32)
   expect_named(batch, c("x", "y"))
 })
+
 test_that("tests for the emnist dataset", {
-  skip_on_cran()
-  
-  dir <- tempfile(fileext = "/")
-  
-  expect_error(
-    ds <- emnist_dataset(dir)
+  dir <- tempfile()
+
+  ds <- emnist_dataset(
+    root = dir,
+    split = "balanced",
+    download = TRUE
   )
 
-  splits <- list(
-    byclass  = list(n_classes = 62, n_train = 697932),
-    bymerge  = list(n_classes = 47, n_train = 697932),
-    balanced = list(n_classes = 47, n_train = 112800),
-    letters  = list(n_classes = 26, n_train = 124800),
-    digits   = list(n_classes = 10, n_train = 240000),
-    mnist    = list(n_classes = 10, n_train = 60000)
+  expect_s3_class(ds, "emnist_dataset")
+  expect_type(ds$.getitem(1), "list")
+  expect_named(ds$.getitem(1), c("x", "y"))
+
+  item <- ds$.getitem(1)
+  expect_equal(length(dim(item$x)), 2)
+  expect_equal(dim(as.array(item$x)), c(28, 28))
+  expect_true(item$y >= 1 && item$y <= length(ds$classes))
+
+  ds2 <- emnist_dataset(
+    root = dir,
+    split = "balanced",
+    transform = transform_to_tensor
   )
-
-  test_split <- "balanced"
-  info <- splits[[test_split]]
-  
-  ds <- emnist_dataset(dir, split = test_split, download = TRUE)
-  
-  i <- ds[1]
-  expect_equal(dim(i[[1]]), c(28, 28))
-  expect_true(i[[2]] %in% 0:(info$n_classes - 1))
-  expect_equal(length(ds), info$n_train)
-
-  ds_transformed <- emnist_dataset(dir, split = test_split, transform = transform_to_tensor)
-  dl <- dataloader(ds_transformed, batch_size = 32)
-  expect_length(dl, ceiling(info$n_train / 32))
-
+  dl <- torch::dataloader(ds2, batch_size = 32)
   iter <- dataloader_make_iter(dl)
   batch <- dataloader_next(iter)
 
-  expect_tensor_shape(batch[[1]], c(32, 1, 28, 28))
-  expect_tensor_shape(batch[[2]], 32)
-  expect_true((torch_max(batch[[1]]) <= 1)$item())
+  expect_tensor_shape(batch$x, c(32, 1, 28, 28))
+  expect_tensor_shape(batch$y, 32)
   expect_named(batch, c("x", "y"))
-  
 })
+
 
 test_that("tests for the qmnist dataset", {
   dir <- tempfile(fileext = "/")
