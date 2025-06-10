88b894f5e80d5add44908a5d2b4d993b0b986b41
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-11 00:57:37 +0530

    Addition of dataset loader tests for FGVC Dataset #134

--- tests/testthat/test-dataset-fgvc.R
+++ tests/testthat/test-dataset-fgvc.R
@@ -16,6 +16,27 @@ test_that("FGVC-Aircraft dataset: all splits, levels, and dataloader", {
   expect_type(item$y, "integer")
   expect_equal(as.numeric(item$y), 79)
 
+  resize_collate_fn <- function(batch) {
+    xs <- lapply(batch, function(sample) {
+      torchvision::transform_resize(sample$x, c(224, 224))
+    })
+    xs <- torch::torch_stack(xs)
+    ys <- torch::torch_tensor(sapply(batch, function(sample) sample$y), dtype = torch::torch_long())
+    list(x = xs, y = ys)
+  }
+  dl <- torch::dataloader(
+    dataset = fgvc,
+    batch_size = 2,
+    shuffle = TRUE,
+    collate_fn = resize_collate_fn
+  )
+  iter <- dataloader_make_iter(dl)
+  batch <- dataloader_next(iter)
+  expect_named(batch, c("x", "y"))
+  expect_true(inherits(batch$x, "torch_tensor"))
+  expect_equal(dim(batch$x)[1], 2)
+  expect_equal(length(batch$y), 2)
+
   fgvc <- fgvc_aircraft_dataset(root = t, split = "val", annotation_level = "variant")
   expect_equal(length(fgvc), 1334)
   expect_equal(as.numeric(fgvc[1]$y), 65)
