43c820ac3f3d194035a2b0fe9ef92245b486b7ee
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-10 17:11:18 +0530

    Updating the default root to tempdir() for mnist kmnist and fashion-mnist datasets

--- R/dataset-mnist.R
+++ R/dataset-mnist.R
@@ -28,7 +28,7 @@ mnist_dataset <- dataset(
   test_file = 'test.rds',
   classes = c('0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
              '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine'),
-  initialize = function(root, train = TRUE, transform = NULL, target_transform = NULL,
+  initialize = function(root = tempdir(), train = TRUE, transform = NULL, target_transform = NULL,
                         download = FALSE) {
 
     self$root_path <- root
