21c5b7d2396bd656334cbd14c71ea69a6acae3ec
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-11 09:07:29 +0530

    Making the tests more comprehensive

--- tests/testthat/test-dataset-mnist.R
+++ tests/testthat/test-dataset-mnist.R
@@ -86,30 +86,35 @@ test_that("tests for the emnist dataset", {
   expect_equal(length(emnist), 112800)
   first_item <- emnist[1]
   expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "array"))
   expect_equal((first_item[[2]]), 46)
 
   emnist <- emnist_dataset(dir, split = "byclass")
   expect_equal(length(emnist), 697932)
   first_item <- emnist[1]
   expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "array"))
   expect_equal((first_item[[2]]), 36)
 
   emnist <- emnist_dataset(dir, split = "bymerge")
   expect_equal(length(emnist), 697932)
   first_item <- emnist[1]
   expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "array"))
   expect_equal((first_item[[2]]), 25)
 
   emnist <- emnist_dataset(dir, split = "letters")
   expect_equal(length(emnist), 124800)
   first_item <- emnist[1]
   expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "array"))
   expect_equal((first_item[[2]]), 24)
 
   emnist <- emnist_dataset(dir, split = "digits")
   expect_equal(length(emnist), 240000)
   first_item <- emnist[1]
   expect_named(first_item, c("x", "y"))
+  expect_true(inherits(first_item$x, "array"))
   expect_equal((first_item[[2]]), 9)
 
   emnist <- emnist_dataset(dir, split = "mnist")
