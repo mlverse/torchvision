dd366c277ad6ae19f12a048ce3b5a5122d96d864
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-17 00:23:05 +0530

    Update: Comprehensive tests for Caltech256 #128

--- tests/testthat/test-dataset-caltech.R
+++ tests/testthat/test-dataset-caltech.R
@@ -71,25 +71,48 @@ test_that("Caltech101 dataset works correctly", {
   expect_equal(batch$y[[4]],"accordion")
 })
 
-# test_that("Caltech256 dataset works correctly", {
-#   t <- tempfile()
+test_that("Caltech256 dataset works correctly", {
+  t <- tempdir()
 
-#   expect_error(
-#     caltech256_dataset(root = t, download = FALSE),
-#     class = "runtime_error"
-#   )
+  expect_error(
+    caltech256_dataset(root = tempfile(), download = FALSE),
+    class = "runtime_error"
+  )
 
-#   caltech256 <- caltech256_dataset(root = t, download = TRUE)
-#   expect_equal(length(caltech256), 30607)
-#   first_item <- caltech256[1]
-#   expect_named(first_item, c("x", "y"))
-#   expect_true(inherits(first_item$x, "torch_tensor"))
-#   expect_equal(first_item$y, "001.ak47")
+  caltech256 <- caltech256_dataset(root = t, download = TRUE)
+  expect_length(caltech256, 30607)
+  first_item <- caltech256[1]
+  expect_named(first_item, c("x", "y"))
+  expect_length(first_item$x,416166)
+  expect_type(first_item$x, "integer")
+  expect_type(first_item$y,"character")
+  expect_equal(first_item$y, "ak47")
 
-#   dl <- dataloader(caltech256, batch_size = 1)
-#   iter <- dataloader_make_iter(dl)
-#   batch <- dataloader_next(iter)
-#   expect_true(inherits(batch$x, "torch_tensor"))
-#   expect_equal(batch$x$shape[1], 1)
-#   expect_equal(batch$y, "001.ak47")
-# })
+  resize_collate_fn <- function(batch) {
+    target_size <- c(224, 224)
+    xs <- lapply(batch, function(sample) {
+      torchvision::transform_resize(sample$x, target_size)
+    })
+    xs <- torch::torch_stack(lapply(xs, torch::torch_tensor))
+    ys <- lapply(batch, function(sample) sample$y)
+    list(x = xs, y = ys)
+  }
+  ds <- caltech256_dataset(
+    root = t,
+    transform = transform_to_tensor
+  )
+  dl <- dataloader(ds, batch_size = 4, collate_fn = resize_collate_fn)
+  iter <- dataloader_make_iter(dl)
+  batch <- dataloader_next(iter)
+  expect_length(batch, 2)
+  expect_tensor(batch$x)
+  expect_length(batch$x,602112)
+  expect_tensor_shape(batch$x,c(4,3,224,224))
+  expect_tensor_dtype(batch$x,torch_float())
+  expect_type(batch$y,"list")
+  expect_type(batch$y[[1]],"character")
+  expect_equal(batch$y[[1]],"ak47")
+  expect_equal(batch$y[[2]],"ak47")
+  expect_equal(batch$y[[3]],"ak47")
+  expect_equal(batch$y[[4]],"ak47")
+})
