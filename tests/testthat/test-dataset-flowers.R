260959609fdfb63709b14dcc6f2de6f4833d0649
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-23 01:17:07 +0530

    Updating tests and splitting into batches for better debugging #136

--- tests/testthat/test-dataset-flowers.R
+++ tests/testthat/test-dataset-flowers.R
@@ -2,11 +2,11 @@ context("dataset-flowers")
 
 t <- withr::local_tempdir()
 
-test_that("tests for the Flowers102 dataset", {
+test_that("tests for the Flowers102 dataset for train split", {
 
   expect_error(
     flowers102_dataset(root = tempfile(), download = FALSE),
-    class = "runtime_error"
+    class = "rlang_error"
   )
 
   flowers <- flowers102_dataset(root = t, split = "train", download = TRUE)
@@ -14,38 +14,50 @@ test_that("tests for the Flowers102 dataset", {
   first_item <- flowers[1]
   expect_length(first_item$x,1131000)
   expect_named(first_item, c("x", "y"))
-  expect_type(first_item$x, "integer")
+  expect_type(first_item$x, "double")
   expect_type(first_item$y,"integer")
   expect_equal(first_item$y, 1)
+})
+
+test_that("tests for the Flowers102 dataset for test split", {
 
   flowers <- flowers102_dataset(root = t, split = "test", download = TRUE)
   expect_length(flowers, 6149)
   first_item <- flowers[1]
   expect_length(first_item$x,784500)
   expect_named(first_item, c("x", "y"))
-  expect_type(first_item$x, "integer")
+  expect_type(first_item$x, "double")
   expect_type(first_item$y,"integer")
   expect_equal(first_item$y, 1)
+})
+
+test_that("tests for the Flowers102 dataset for validation split", {
 
   flowers <- flowers102_dataset(root = t, split = "val", download = TRUE)
   expect_length(flowers, 1020)
   first_item <- flowers[1]
   expect_length(first_item$x,909000)
   expect_named(first_item, c("x", "y"))
-  expect_type(first_item$x, "integer")
+  expect_type(first_item$x, "double")
   expect_type(first_item$y,"integer")
   expect_equal(first_item$y, 1)
+})
+
+test_that("tests for the Flowers102 dataset for dataloader", {
 
   resize_collate_fn <- function(batch) {
+    target_size <- c(224, 224)
     xs <- lapply(batch, function(sample) {
-      torchvision::transform_resize(sample$x, c(224, 224))
+      img <- sample$x
+      img <- torch_tensor(img)
+      transform_resize(img, target_size)
     })
     xs <- torch::torch_stack(xs)
-    ys <- sapply(batch, function(sample) sample$y)
+    ys <- lapply(batch, function(sample) sample$y)
     list(x = xs, y = ys)
   }
   dl <- torch::dataloader(
-    dataset = flowers102_dataset(root = t, transform = transform_to_tensor),
+    dataset = flowers102_dataset(root = t),
     batch_size = 4,
     collate_fn = resize_collate_fn
   )
@@ -56,11 +68,10 @@ test_that("tests for the Flowers102 dataset", {
   expect_length(batch$x,602112)
   expect_tensor_shape(batch$x,c(4,3,224,224))
   expect_tensor_dtype(batch$x,torch_float())
-  expect_type(batch$y,"integer")
+  expect_type(batch$y,"list")
   expect_length(batch$y, 4)
-  expect_equal(batch$y[1],1)
-  expect_equal(batch$y[2],1)
-  expect_equal(batch$y[3],1)
-  expect_equal(batch$y[4],1)
-
+  expect_equal(batch$y[[1]],1)
+  expect_equal(batch$y[[2]],1)
+  expect_equal(batch$y[[3]],1)
+  expect_equal(batch$y[[4]],1)
 })
