518bb172be6dad38edc00a21f6fd4a80561ac3e3
Akanksha Koshti <koshtiakanksha12@gmail.com>
2025-06-27 16:00:38 -0700

    updating convert-models with new links

--- tests/testthat/test-models-efficientnet.R
+++ tests/testthat/test-models-efficientnet.R
@@ -1,116 +1,30 @@
-test_that("efficientnet_b0", {
-  model <- model_efficientnet_b0()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  model <- model_efficientnet_b0(pretrained = TRUE)
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
-
-test_that("efficientnet_b1", {
+test_that("efficientnet models work", {
   skip_on_os(c("windows", "mac"))
 
-  model <- model_efficientnet_b1()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
+  models <- list(
+    model_efficientnet_b0,
+    model_efficientnet_b1,
+    model_efficientnet_b2,
+    model_efficientnet_b3,
+    model_efficientnet_b4,
+    model_efficientnet_b5,
+    model_efficientnet_b6,
+    model_efficientnet_b7
+  )
 
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b1(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
+  for (m in models) {
+    model <- m()
+    input <- torch::torch_randn(1, 3, 256, 256)
+    out <- model(input)
+    expect_tensor_shape(out, c(1, 1000))
+  }
 
-test_that("efficientnet_b2", {
-  skip_on_os(c("windows", "mac"))
-
-  model <- model_efficientnet_b2()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b2(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
-
-test_that("efficientnet_b3", {
-  skip_on_os(c("windows", "mac"))
-
-  model <- model_efficientnet_b3()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b3(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
-
-test_that("efficientnet_b4", {
-  skip_on_os(c("windows", "mac"))
-
-  model <- model_efficientnet_b4()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b4(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
-
-test_that("efficientnet_b5", {
-  skip_on_os(c("windows", "mac"))
-
-  model <- model_efficientnet_b5()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b5(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
-
-test_that("efficientnet_b6", {
-  skip_on_os(c("windows", "mac"))
-
-  model <- model_efficientnet_b6()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b6(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-})
-
-test_that("efficientnet_b7", {
-  skip_on_os(c("windows", "mac"))
-
-  model <- model_efficientnet_b7()
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
-
-  withr::with_options(list(timeout = 360),
-                      model <- model_efficientnet_b7(pretrained = TRUE))
-  input <- torch::torch_randn(1, 3, 256, 256)
-  out <- model(input)
-  expect_tensor_shape(out, c(1, 1000))
+  for (m in models) {
+    withr::with_options(list(timeout = 360), model <- m(pretrained = TRUE))
+    input <- torch::torch_randn(1, 3, 256, 256)
+    out <- model(input)
+    expect_tensor_shape(out, c(1, 1000))
+    rm(model)
+    gc()
+  }
 })
