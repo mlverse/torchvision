cd1e0aa89e4e550406dde3652b1fbed49be6c995
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-28 20:15:50 +0530

    Cleanup for eurosat

--- tests/testthat/test-dataset-eurosat.R
+++ tests/testthat/test-dataset-eurosat.R
@@ -1,4 +1,4 @@
-temp_root <- tempfile(fileext = "/")
+temp_root <- withr::local_tempdir()
 
 test_that("eurosat_dataset downloads correctly whatever the split", {
   skip_on_cran()
@@ -6,7 +6,7 @@ test_that("eurosat_dataset downloads correctly whatever the split", {
 
 
   expect_error(
-    eurosat_dataset(root = temp_root, split = "test", download = FALSE),
+    eurosat_dataset(root = tempfile(), split = "test", download = FALSE),
     "Dataset not found. You can use `download = TRUE`",
     label = "Dataset should fail if not previously downloaded"
   )
@@ -17,9 +17,10 @@ test_that("eurosat_dataset downloads correctly whatever the split", {
 
   expect_is(ds, "dataset", "train should be a dataset")
 
-  extracted_dir <- list.dirs(paste0(temp_root,"eurosat/images/2750"), recursive = TRUE, full.names = TRUE)
+  extracted_dir <- file.path(temp_root, "eurosat", "images", "2750")
+  extracted_dir <- list.dirs(extracted_dir, recursive = FALSE, full.names = TRUE)
   # Extracted data folder have one folder per category
-  expect_length(extracted_dir, 11)
+  expect_length(extracted_dir, 10)
 
   image_files <- list.files(extracted_dir, pattern = "\\.jpg$", recursive = TRUE, full.names = TRUE)
   expect_gte(length(image_files), 16200, "Image files should be present in the extracted directory")
@@ -64,7 +65,7 @@ test_that("eurosat100_dataset derivatives download and prepare correctly", {
 
 
   expect_error(
-    eurosat100_dataset(root = temp_root, split = "test", download = FALSE),
+    eurosat100_dataset(root = tempfile(), split = "test", download = FALSE),
     "Dataset not found. You can use `download = TRUE`",
     label = "Dataset should fail if not previously downloaded"
   )
@@ -94,7 +95,7 @@ test_that("eurosat_all_bands_dataset derivatives download and prepare correctly"
 
 
   expect_error(
-    eurosat_all_bands_dataset(root = temp_root, split = "test", download = FALSE),
+    eurosat_all_bands_dataset(root = tempfile(), split = "test", download = FALSE),
     "Dataset not found. You can use `download = TRUE`",
     label = "Dataset should fail if not previously downloaded"
   )
