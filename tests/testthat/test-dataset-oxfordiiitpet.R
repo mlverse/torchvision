54d68d2adaba509648a5ff14188b94a8b37d0eda
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-07-06 17:17:27 +0530

    Updating tests for class image_with_segmentation_mask #140

--- tests/testthat/test-dataset-oxfordiiitpet.R
+++ tests/testthat/test-dataset-oxfordiiitpet.R
@@ -4,8 +4,8 @@ t = withr::local_tempdir()
 
 test_that("tests for the Oxford-IIIT Pet Segmentation dataset for train split with target type category", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_segmentation_dataset(root = t, target_type = "category", train = TRUE, download = TRUE)
   expect_length(oxfordiiitpet, 3680)
@@ -17,14 +17,15 @@ test_that("tests for the Oxford-IIIT Pet Segmentation dataset for train split wi
   expect_tensor(first_item$y$mask)
   expect_tensor_shape(first_item$y$mask,c(3,500,394))
   expect_tensor_dtype(first_item$y$mask,torch_bool())
-  expect_type(first_item$y$label,"integer")
+  expect_type(first_item$y$label, "integer")
   expect_equal(first_item$y$label, 1)
+  expect_s3_class(first_item, "image_with_segmentation_mask")
 })
 
 test_that("tests for the Oxford-IIIT Pet Segmentation dataset for train split with target type binary category", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_segmentation_dataset(root = t, target_type = "binary-category", train = TRUE)
   expect_length(oxfordiiitpet, 3680)
@@ -38,12 +39,13 @@ test_that("tests for the Oxford-IIIT Pet Segmentation dataset for train split wi
   expect_tensor_dtype(first_item$y$mask,torch_bool())
   expect_type(first_item$y$label,"integer")
   expect_equal(first_item$y$label, 1)
+  expect_s3_class(first_item, "image_with_segmentation_mask")
 })
 
 test_that("tests for the Oxford-IIIT Pet Segmentation dataset for test split with target type category", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_segmentation_dataset(root = t, target_type = "category", train = FALSE)
   expect_length(oxfordiiitpet, 3669)
@@ -57,12 +59,13 @@ test_that("tests for the Oxford-IIIT Pet Segmentation dataset for test split wit
   expect_tensor_dtype(first_item$y$mask,torch_bool())
   expect_type(first_item$y$label,"integer")
   expect_equal(first_item$y$label, 1)
+  expect_s3_class(first_item, "image_with_segmentation_mask")
 })
 
 test_that("tests for the Oxford-IIIT Pet Segmentation dataset for test split with target type binary category", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_segmentation_dataset(root = t, target_type = "binary-category", train = FALSE)
   expect_length(oxfordiiitpet, 3669)
@@ -76,12 +79,13 @@ test_that("tests for the Oxford-IIIT Pet Segmentation dataset for test split wit
   expect_tensor_dtype(first_item$y$mask,torch_bool())
   expect_type(first_item$y$label,"integer")
   expect_equal(first_item$y$label, 1)
+  expect_s3_class(first_item, "image_with_segmentation_mask")
 })
 
 test_that("tests for the Oxford-IIIT Pet Segmentation dataset for dataloader", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_segmentation_dataset(
     root = t,
@@ -93,6 +97,7 @@ test_that("tests for the Oxford-IIIT Pet Segmentation dataset for dataloader", {
       y
     }
   )
+  expect_s3_class(oxfordiiitpet[1], "image_with_segmentation_mask")
   dl <- dataloader(oxfordiiitpet, batch_size = 4)
   batch <- dataloader_next(dataloader_make_iter(dl))
   expect_named(batch, c("x", "y"))
@@ -117,8 +122,8 @@ test_that("tests for the Oxford-IIIT Pet Segmentation dataset for dataloader", {
 
 test_that("tests for the Oxford-IIIT Pet dataset for train split", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_dataset(root = t, train = TRUE, download = TRUE)
   expect_length(oxfordiiitpet, 3680)
@@ -132,8 +137,8 @@ test_that("tests for the Oxford-IIIT Pet dataset for train split", {
 
 test_that("tests for the Oxford-IIIT Pet dataset for test split", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_dataset(root = t, train = FALSE)
   expect_length(oxfordiiitpet, 3669)
@@ -147,8 +152,8 @@ test_that("tests for the Oxford-IIIT Pet dataset for test split", {
 
 test_that("tests for the Oxford-IIIT Pet dataset for dataloader", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_dataset(
     root = t,
@@ -174,8 +179,8 @@ test_that("tests for the Oxford-IIIT Pet dataset for dataloader", {
 
 test_that("tests for the Oxford-IIIT Pet binary dataset for train split", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_binary_dataset(root = t, train = TRUE)
   expect_length(oxfordiiitpet, 3680)
@@ -189,8 +194,8 @@ test_that("tests for the Oxford-IIIT Pet binary dataset for train split", {
 
 test_that("tests for the Oxford-IIIT Pet binary dataset for test split", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_binary_dataset(root = t, train = FALSE)
   expect_length(oxfordiiitpet, 3669)
@@ -204,8 +209,8 @@ test_that("tests for the Oxford-IIIT Pet binary dataset for test split", {
 
 test_that("tests for the Oxford-IIIT Pet binary dataset for dataloader", {
 
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-        "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+  # skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+  #       "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   oxfordiiitpet <- oxfordiiitpet_binary_dataset(
     root = t,
