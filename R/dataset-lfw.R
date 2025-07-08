c0a1c43db8473b5d67843dfc11b534fee3d9cc6d
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-07-09 00:44:54 +0530

    Added lfw_pairs dataset #142

--- R/dataset-lfw.R
+++ R/dataset-lfw.R
@@ -44,8 +44,8 @@
 lfw_people_dataset <- torch::dataset(
   name = "lfw_people",
   archive_size = "120 MB",
+  base_url = "https://huggingface.co/datasets/JimmyUnleashed/LFW/resolve/main",
   resources = list(
-    base_url = "https://huggingface.co/datasets/JimmyUnleashed/LFW/resolve/main",
     lfw_images = c("lfw-deepfunneled.zip", "e782a3d6f143c8964f531d0a5af1201a"),
     train = c("peopleDevTrain.csv", "ef0a2b842aa55831d7d55b6bb7d450be"),
     test = c("peopleDevTest.csv", "f95f156446aaee28e8fdeb6c9883eee8")
@@ -119,17 +119,11 @@ lfw_people_dataset <- torch::dataset(
 
     cli_inform("Downloading {.cls {class(self)[[1]]}}...")
 
-    resources <- list(
-      self$resources$lfw_images,
-      self$resources$train,
-      self$resources$test
-    )
-
-    for (res in resources) {
+    for (res in self$resources) {
       filename <- res[1]
       expected_md5 <- res[2]
 
-      url <- file.path(self$resources$base_url, filename)
+      url <- file.path(self$base_url, filename)
       archive <- download_and_cache(url, prefix = class(self)[1])
       actual_md5 <- tools::md5sum(archive)
 
@@ -171,5 +165,112 @@ lfw_people_dataset <- torch::dataset(
 
   .length = function() {
     length(self$img_path)
+  }
+)
+
+#' @export
+lfw_pairs_dataset <- torch::dataset(
+  inherit = lfw_people_dataset,
+  name = "lfw_pairs",
+  archive_size = "120 MB",
+  base_url = "https://huggingface.co/datasets/JimmyUnleashed/LFW/resolve/main",
+  resources = list(
+    lfw_images = c("lfw-deepfunneled.zip", "e782a3d6f143c8964f531d0a5af1201a"),
+    match_train = c("matchpairsDevTrain.csv", "f7502e5a6350c7d2b795acf71406bc33"),
+    mismatch_train = c("mismatchpairsDevTrain.csv", "80994187f484fdefb4713a96aa9ddeb6"),
+    match_test = c("matchpairsDevTest.csv", "e4470bc1288afccf7e5cecc3623271a4"),
+    mismatch_test = c("mismatchpairsDevTest.csv", "cb9f0975a0c19306cff2e6bdd7a7a7e3")
+  ),
+
+    initialize = function(
+    root = tempdir(),
+    train = TRUE,
+    transform = NULL,
+    target_transform = NULL,
+    download = FALSE
+  ) {
+    self$root <- root
+    self$train <- train
+    self$transform <- transform
+    self$target_transform <- target_transform
+    self$classes <- c("Different","Same")
+
+    base_url <- self$base_url
+
+    if (download) {
+      cli_inform("Dataset {.cls {class(self)[[1]]}} (~{.emph {self$archive_size}}) will be downloaded and processed if not already available.")
+      self$download()
+    }
+
+    if (!self$check_exists()) {
+      cli_abort("Dataset not found. You can use `download = TRUE` to download it.")
+    }
+
+    if (train) {
+      match_file <- self$resources$match_train[1]
+      mismatch_file <- self$resources$mismatch_train[1]
+    } else {
+      match_file <- self$resources$match_test[1]
+      mismatch_file <- self$resources$mismatch_test[1]
+    }
+
+    match_path <- file.path(root, match_file)
+    mismatch_path <- file.path(root, mismatch_file)
+
+    match_df <- read.csv(match_path)
+    mismatch_df <- read.csv(mismatch_path)
+
+    matched <- data.frame(
+      img1 = file.path(match_df$name, sprintf("%s_%04d.jpg", match_df$name, match_df$imagenum1)),
+      img2 = file.path(match_df$name, sprintf("%s_%04d.jpg", match_df$name, match_df$imagenum2)),
+      same = 1
+    )
+
+    mismatched <- data.frame(
+      img1 = file.path(mismatch_df$name, sprintf("%s_%04d.jpg", mismatch_df$name, mismatch_df$imagenum1)),
+      img2 = file.path(mismatch_df$name.1, sprintf("%s_%04d.jpg", mismatch_df$name.1, mismatch_df$imagenum2)),
+      same = 0
+    )
+
+    self$pairs <- rbind(matched, mismatched)
+    self$img_path <- c(self$pairs$img1, self$pairs$img2)
+    cli_inform("{.cls {class(self)[[1]]}} dataset loaded with {length(self$img_path)} images across {length(self$classes)} classes.")
+  },
+
+
+  .getitem = function(index) {
+    pair <- self$pairs[index, ]
+
+    img1_path <- file.path(self$root, "lfw-deepfunneled", pair$img1)
+    img2_path <- file.path(self$root, "lfw-deepfunneled", pair$img2)
+
+    x1 <- jpeg::readJPEG(img1_path)
+    x2 <- jpeg::readJPEG(img2_path)
+    x <- list(x1,x2)
+    y <- as.integer(pair$same)
+
+    if (!is.null(self$transform)) {
+      x <- self$transform(x)
+    }
+
+    if (!is.null(self$target_transform)) {
+      y <- self$target_transform(y)
+    }
+
+    list(x = x, y = y)
+  },
+
+  .length = function() {
+    nrow(self$pairs)
+  },
+
+  check_exists = function() {
+    required_files <- c(
+      self$resources$match_train[1],
+      self$resources$mismatch_train[1],
+      self$resources$match_test[1],
+      self$resources$mismatch_test[1]
+    )
+    fs::dir_exists(file.path(self$root, "lfw-deepfunneled")) && all(fs::file_exists(file.path(self$root, required_files)))
   }
 )
