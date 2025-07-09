46729ebe649011927f94c6765c72799bd966cbac
Akanksha Koshti <koshtiakanksha12@gmail.com>
2025-07-09 14:21:32 -0700

    Add model_fcn_resnet50 and model_fcn_resnet_101 implementations with tests and docs.

---
+++ tests/testthat/test-models-fcn.R
@@ -0,0 +1,27 @@
+test_that("model_fcn_resnet50 returns correct output shape", {
+  model <- model_fcn_resnet50(pretrained = FALSE, num_classes = 21)
+  model$eval()
+
+  input <- torch::torch_randn(1, 3, 224, 224)
+  output <- model(input)
+
+  expect_named(output, c("out"))
+  expect_tensor_shape(output$out, c(1, 21, 224, 224))
+})
+
+test_that("model_fcn_resnet50 with aux classifier returns aux output", {
+  model <- model_fcn_resnet50(pretrained = FALSE, num_classes = 21, aux_loss = TRUE)
+  model$eval()
+
+  input <- torch::torch_randn(1, 3, 224, 224)
+  output <- model(input)
+
+  expect_named(output, c("out", "aux"))
+  expect_tensor_shape(output$out, c(1, 21, 224, 224))
+  expect_tensor_shape(output$aux, c(1, 21, 224, 224))
+})
+
+test_that("model_fcn_resnet50 loads pretrained weights", {
+  model <- model_fcn_resnet50(pretrained = TRUE)
+  expect_true(inherits(model, "fcn"))
+})
