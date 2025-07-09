303d0798ab972e516abd0b2c5de0e348b6cdb023
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-07-10 03:53:19 +0530

    Addition of num_classes tests #145

--- tests/testthat/test-models-vit.R
+++ tests/testthat/test-models-vit.R
@@ -12,6 +12,11 @@ test_that("tests for model_vit_b_16", {
   expect_tensor_shape(out, c(1, 1000))
   #expect_equal_to_r(out[1, 1], -0.87923414, tol = 1e-8)
 
+  model <- model_vit_b_16(num_classes = 42)
+  input <- torch::torch_randn(1, 3, 224, 224)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 42))
+
   rm(model)
   gc()
 })
@@ -29,6 +34,11 @@ test_that("tests for model_vit_b_32", {
   out <- model(input)
   expect_tensor_shape(out, c(1, 1000))
   #expect_equal_to_r(out[1, 1], -0.54261702, tol = 1e-8)
+
+  model <- model_vit_b_32(num_classes = 42)
+  input <- torch::torch_randn(1, 3, 224, 224)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 42))
 
   rm(model)
   gc()
@@ -48,6 +58,11 @@ test_that("tests for model_vit_l_16", {
   expect_tensor_shape(out, c(1, 1000))
   #expect_equal_to_r(out[1, 1], -0.78047544, tol = 1e-8)
 
+  model <- model_vit_l_16(num_classes = 42)
+  input <- torch::torch_randn(1, 3, 224, 224)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 42))
+
   rm(model)
   gc()
 })
@@ -66,6 +81,11 @@ test_that("tests for model_vit_l_32", {
   expect_tensor_shape(out, c(1, 1000))
   #expect_equal_to_r(out[1, 1], -0.78083247, tol = 1e-8)
 
+  model <- model_vit_l_32(num_classes = 42)
+  input <- torch::torch_randn(1, 3, 224, 224)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 42))
+
   rm(model)
   gc()
 })
@@ -83,6 +103,11 @@ test_that("tests for model_vit_h_14", {
   out <- model(input)
   expect_tensor_shape(out, c(1, 1000))
   #expect_equal_to_r(out[1, 1], -0.98756719, tol = 1e-8)
+
+  model <- model_vit_h_14(num_classes = 42)
+  input <- torch::torch_randn(1, 3, 518, 518)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 42))
 
   rm(model)
   gc()
