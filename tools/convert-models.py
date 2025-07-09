e870578a5ca15a17cfe56e47478bcb82d3fde70e
Daniel Falbel <dfalbel@gmail.com>
2025-07-09 10:26:32 -0300

    update scripts to upload converted models (#205)

--- tools/convert-models.py
+++ tools/convert-models.py
@@ -1,19 +1,20 @@
 import torch
 from torch.hub import load_state_dict_from_url # used to be in torchvision
-from google.cloud import storage
 import os
+import boto3
+
+s3 = boto3.resource('s3')
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # bucket_name = "your-bucket-name"
     # source_file_name = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    blob.upload_from_filename(source_file_name)
+    s3.upload_file(
+      source_file_name, 
+      bucket_name, 
+      destination_blob_name
+    )
 
     print(
         "File {} uploaded to {}.".format(
@@ -85,7 +86,7 @@ for name, url in models.items():
   fpath = "models/" + name + ".pth"
   torch.save(converted, fpath, _use_new_zipfile_serialization=True)
   upload_blob(
-    "torchvision-models",
+    "torch-pretrained-models",
     fpath,
-    "v2/" + fpath
+    "models/vision/v2/" + fpath
   )
