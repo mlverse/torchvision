ae4b5f7d9f69e98b341d0ef0062c2326ef3b64cc
cregouby <cregouby@users.noreply.github.com>
2025-07-23 09:33:21 +0200

    add a test for proper image segmentation

--- tests/testthat/test-models-fcn.R
+++ tests/testthat/test-models-fcn.R
@@ -44,6 +44,27 @@ test_that("model_fcn_resnet50 loads pretrained weights", {
   expect_tensor_shape(output$aux, c(2, 21, 224, 224))
 })
 
+test_that("model_fcn_resnet50 can segment a cat", {
+  voc_idx <- setNames(seq_along(torchvision:::voc_segmentation_classes), torchvision:::voc_segmentation_classes)
+
+  model <- model_fcn_resnet50(pretrained = TRUE)
+  img <- torch::torch_tensor(jpeg::readJPEG("assets/class/cat/cat.1.jpg"))$permute(c(3,1,2))
+  norm_mean <- c(0.485, 0.456, 0.406) #ImageNet normalization constants
+  norm_std  <- c(0.229, 0.224, 0.225)
+  input <- img %>% transform_resize(c(520, 520)) %>% transform_normalize(input, norm_mean, norm_std)
+
+  output <- model(input$unsqueeze(1))
+  mask_id <- output$out$argmax(dim = 2)
+  mask_table <- factor(mask_id %>% torch::as_array(), levels = voc_idx, labels = names(voc_idx)) %>% table()
+
+  expect_gt(mask_table[["cat"]], 0)
+  expect_gt(mask_table[["cat"]], mask_table[["dog"]])
+  expect_gt(mask_table[["cat"]], mask_table[["person"]])
+
+  expect_gt(mask_table[["background"]], 0)
+
+})
+
 
 test_that("model_fcn_resnet101 returns correct output shape", {
   model <- model_fcn_resnet101(pretrained = FALSE, num_classes = 21)
