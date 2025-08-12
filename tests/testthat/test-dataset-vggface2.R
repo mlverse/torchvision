954b0c4a82c1f00eb4154c39af51a9b49f40781c
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-08-13 00:40:12 +0530

    Adding skips to tests, tests sucessful on local #224

--- tests/testthat/test-dataset-vggface2.R
+++ tests/testthat/test-dataset-vggface2.R
@@ -4,6 +4,9 @@ t <- withr::local_tempdir()
 
 test_that("VGGFace2 dataset works correctly for train split", {
 
+  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+          "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+
   vgg <- vggface2_dataset(root = t, download = TRUE)
   expect_length(vgg, 3141890)
   first_item <- vgg[1]
@@ -14,6 +17,9 @@ test_that("VGGFace2 dataset works correctly for train split", {
 })
 
 test_that("VGGFace2 dataset works correctly for test split", {
+
+  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+          "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   vgg <- vggface2_dataset(root = t, train = FALSE)
   expect_length(vgg, 169396)
