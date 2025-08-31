fc5581cf5a0d66a746592ffdece270ac3c771051
C. Regouby <christophe.regouby@free.fr>
2025-08-31 08:04:12 +0200

    move to selective downloads (WiP)

--- R/dataset-vggface2.R
+++ R/dataset-vggface2.R
@@ -11,7 +11,7 @@
 #' @return A torch dataset object `vggface2_dataset`:
 #' - `x`: RGB image array.
 #' - `y`: Integer label (1…N) for the identity.
-#' 
+#'
 #' `ds$classes` is a named list mapping integer labels to a list with:
 #' - `name`: Character name of the person.
 #' - `gender`: "Male" or "Female".
@@ -37,35 +37,36 @@
 #' @export
 vggface2_dataset <- torch::dataset(
   name = "vggface2",
-  resources = list(
-    train_images = c("https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/data/vggface2_train.tar.gz","88813c6b15de58afc8fa75ea83361d7f"),
-    test_images  = c("https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/data/vggface2_test.tar.gz","bb7a323824d1004e14e00c23974facd3"),
-    train_list   = c("https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/meta/train_list.txt","4cfbab4a839163f454d7ecef28b68669"),
-    test_list    = c("https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/meta/test_list.txt","d08b10f12bc9889509364ef56d73c621"),
-    identity     = c("https://huggingface.co/datasets/ProgramComputer/VGGFace2/raw/main/meta/identity_meta.csv","d315386c7e8e166c4f60e27d9cc61acc")
+  resources = data.frame(
+    split = c("train", "train", "test", "test", "identity"),
+    kind = c("image", "label", "image", "label", "label"),
+    url = c("https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/data/vggface2_train.tar.gz",
+            "https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/meta/train_list.txt",
+            "https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/data/vggface2_test.tar.gz",
+            "https://huggingface.co/datasets/ProgramComputer/VGGFace2/resolve/main/meta/test_list.txt",
+            "https://huggingface.co/datasets/ProgramComputer/VGGFace2/raw/main/meta/identity_meta.csv"),
+    md5 = c("88813c6b15de58afc8fa75ea83361d7f",
+            "4cfbab4a839163f454d7ecef28b68669",
+            "bb7a323824d1004e14e00c23974facd3",
+            "d08b10f12bc9889509364ef56d73c621",
+            "d315386c7e8e166c4f60e27d9cc61acc"),
+    size = c(2.03e9, 62e6, 37.9e9, 3e6, 335e3)
   ),
-  archive_size = "38 GB",
-  training_file = "train.rds",
-  test_file = "test.rds",
-
   initialize = function(
     root = tempdir(),
-    train = TRUE,
+    split = "val",
     transform = NULL,
     target_transform = NULL,
     download = FALSE
   ) {
-    self$root_path <- root
-    self$train <- train
+    self$root <- root
+    self$split <- match.arg(split, c("train", "test"))
     self$transform <- transform
     self$target_transform <- target_transform
-    if (train) {
-      self$split <- "train"
-      self$archive_size <- "36 GB"
-    } else {
-      self$split <- "test"
-      self$archive_size <- "2 GB"
-    }
+    self$archive_url <- self$resources[self$resources$split %in% c(split, "identity"),]$url
+    self$archive_size <- prettyunits::pretty_bytes(sum(self$resources[self$resources$split %in% c(split, "identity"),]$size))
+    self$archive_md5 <- self$resources[self$resources$split %in% c(split, "identity"),]$md5
+    self$split_file <- sapply(self$archive_url, \(x) file.path(rappdirs::user_cache_dir("torch"), class(self)[1], basename(x)))
 
     if (download) {
       cli_inform("Dataset {.cls {class(self)[[1]]}} (~{.emph {self$archive_size}}) will be downloaded and processed if not already available.")
@@ -76,12 +77,8 @@ vggface2_dataset <- torch::dataset(
       cli_abort("Dataset not found. You can use `download = TRUE` to download it.")
     }
 
-    if (train) {
-      data_file <- self$training_file
-    } else {
-      data_file <- self$test_file
-    }
-    data <- readRDS(file.path(self$processed_folder, data_file))
+
+    data <- readRDS(file.path(self$processed_folder, paste0(self$split,"Rds")))
 
     self$img_path <- data$img_path
     self$labels <- data$labels
@@ -91,87 +88,40 @@ vggface2_dataset <- torch::dataset(
   },
 
   download = function() {
-    if (self$check_exists()) {
-      return()
-    }
-
-    fs::dir_create(self$raw_folder)
-    fs::dir_create(self$processed_folder)
+    if (self$check_exists())
+      return(NULL)
 
     cli_inform("Downloading {.cls {class(self)[[1]]}}...")
 
-    archive <- download_and_cache(self$resources$train_images[1], prefix = class(self)[1])
-    if (tools::md5sum(archive) != self$resources$train_images[2]) {
+    # Download and extract archives
+    archive <- sapply(self$archive_url, \(x)  download_and_cache(x, prefix = class(self)[1]))
+    if (!all(tools::md5sum(archive) == self$archive_md5))
       runtime_error("Corrupt file! Delete the file in {archive} and try again.")
+    for (file_id in seq_along(archive)) {
+      fs::file_move(archive[file_id], self$split_file[file_id])
     }
-    utils::untar(archive, exdir = self$raw_folder)
+    utils::untar(archive[[1]], exdir = self$root)
 
-    archive <- download_and_cache(self$resources$test_images[1], prefix = class(self)[1])
-    if (tools::md5sum(archive) != self$resources$test_images[2]) {
-      runtime_error("Corrupt file! Delete the file in {archive} and try again.")
-    }
-    utils::untar(archive, exdir = self$raw_folder)
-
-    archive <- download_and_cache(self$resources$train_list[1], prefix = "train_list")
-    if (tools::md5sum(archive) != self$resources$train_list[2]) {
-      runtime_error("Corrupt file! Delete the file in {archive} and try again.")
-    }
-    fs::file_move(archive, self$raw_folder)
-
-    archive <- download_and_cache(self$resources$test_list[1], prefix = "test_list")
-    if (tools::md5sum(archive) != self$resources$test_list[2]) {
-      runtime_error("Corrupt file! Delete the file in {archive} and try again.")
-    }
-    fs::file_move(archive, self$raw_folder)
-
-    archive <- download_and_cache(self$resources$identity[1], prefix = "identity_meta")
-    if (tools::md5sum(archive) != self$resources$identity[2]) {
-      runtime_error("Corrupt file! Delete the file in {archive} and try again.")
-    }
-    fs::file_move(archive, self$raw_folder)
-    identity_file <- file.path(self$raw_folder, "identity_meta.csv")
-    identity_df <- read.csv(identity_file, sep = ",", stringsAsFactors = FALSE, strip.white = TRUE)
+    identity_df <- read.csv(archive[[3]], sep = ",", stringsAsFactors = FALSE, strip.white = TRUE)
     identity_df$Class_ID <- trimws(identity_df$Class_ID)
-    identity_map <- setNames(
-      lapply(seq_len(nrow(identity_df)), function(i) {
-        if(identity_df$Gender[i] == 'f'){
-          gender <- "Female"
-        }else{
-          gender <- "Male"
-        }
-        list(
-          name = identity_df$Name[i],
-          gender = gender
-        )
-      }),
-      identity_df$Class_ID
-    )
+    identity_df$Gender <- factor(identity_df$Gender, labels = c("Female", "Male"))
 
-    for (split in c("train", "test")) {
-        if (split == "train") {
-          list_file <- file.path(self$raw_folder, "train_list.txt")
-        } else {
-          list_file <- file.path(self$raw_folder, "test_list.txt")
-        }
-        files <- readLines(list_file)
-
-        img_path <- file.path(self$raw_folder, split, files)
-        class_ids <- sub("/.*$", "", files)
-        unique_ids <- unique(class_ids)
-
-        class_to_idx <- setNames(seq_along(unique_ids), unique_ids)
-
-        labels <- as.integer(class_to_idx[class_ids])
-
-        classes_list <- lapply(unique_ids, function(cid) {
-            identity_map[[cid]]
-        })
-
+    files <- read.delim(archive[[2]], sep = "/", col.names = c("Class_ID", "img_path"), header = F)
+    files$img_path <- file.path(self$raw_folder, self$split, files$img_path)
+        # class_ids <- sub("/.*$", "", files)
+        # unique_ids <- unique(class_ids)
+        #
+        # class_to_idx <- setNames(seq_along(unique_ids), unique_ids)
+        #
+        # labels <- as.integer(class_to_idx[class_ids])
+        #
+        # classes_list <- lapply(unique_ids, function(cid) {
+        #     identity_map[[cid]]
+        # })
+        #
         saveRDS(
             list(
-            img_path = img_path,
-            labels = labels,
-            classes = classes_list
+            files
             ),
             file.path(self$processed_folder, paste0(split, ".rds"))
         )
@@ -181,8 +131,7 @@ vggface2_dataset <- torch::dataset(
   },
 
   check_exists = function() {
-    fs::file_exists(file.path(self$processed_folder, self$training_file)) &&
-      fs::file_exists(file.path(self$processed_folder, self$test_file))
+    all(fs::file_exists(self$split_file))
   },
 
   .getitem = function(index) {
--- tests/testthat/test-dataset-vggface2.R
+++ tests/testthat/test-dataset-vggface2.R
@@ -3,27 +3,27 @@ context('dataset-vggface2')
 t <- withr::local_tempdir()
 options(timeout = 60000)
 
-test_that("VGGFace2 dataset works correctly for train split", {
-
-  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
-          "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
-
-  vgg <- vggface2_dataset(root = t, download = TRUE)
-  expect_length(vgg, 3141890)
-  first_item <- vgg[1]
-  expect_named(first_item, c("x", "y"))
-  expect_type(first_item$x, "double")
-  expect_type(first_item$y, "integer")
-  expect_equal(first_item$y, 1)
-})
 
 test_that("VGGFace2 dataset works correctly for test split", {
 
   skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
-  vgg <- vggface2_dataset(root = t, train = FALSE)
+  vgg <- vggface2_dataset(root = t, split = "test", download = TRUE)
   expect_length(vgg, 169396)
+  first_item <- vgg[1]
+  expect_named(first_item, c("x", "y"))
+  expect_type(first_item$x, "double")
+  expect_type(first_item$y, "integer")
+  expect_equal(first_item$y, 1)
+})
+test_that("VGGFace2 dataset works correctly for train split", {
+
+  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = 0) != 1,
+          "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
+
+  vgg <- vggface2_dataset(root = t, split = "train", download = TRUE)
+  expect_length(vgg, 3141890)
   first_item <- vgg[1]
   expect_named(first_item, c("x", "y"))
   expect_type(first_item$x, "double")
