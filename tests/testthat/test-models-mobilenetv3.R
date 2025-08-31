3ad2d7d065d1e503b628e7c1dc86add2fc831519
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-08-31 20:17:26 +0530

    Adding tests with varied width_mult & non-divisible input shapes #200:

--- tests/testthat/test-models-mobilenetv3.R
+++ tests/testthat/test-models-mobilenetv3.R
@@ -2,13 +2,13 @@ context("models-mobilenetv3")
 
 test_that("tests for non-pretrained model_mobilenet_v3_large", {
   model_large <- model_mobilenet_v3_large(pretrained = FALSE)
-  input <- torch::torch_randn(1, 3, 224, 224)
+  input <- torch_randn(1, 3, 224, 224)
   model_large$eval()
   out <- model_large(input)
   expect_tensor_shape(out, c(1000))
 
   model <- model_mobilenet_v3_large(pretrained = FALSE,num_classes = 10)
-  input <- torch::torch_randn(1, 3, 224, 224)
+  input <- torch_randn(1, 3, 224, 224)
   out <- model(input)
   expect_tensor_shape(out, c(10))
 
@@ -19,7 +19,7 @@ test_that("tests for non-pretrained model_mobilenet_v3_large", {
 
 test_that("tests for pretrained model_mobilenet_v3_large", {
   model_large <- model_mobilenet_v3_large(pretrained = TRUE)
-  input <- torch::torch_randn(1, 3, 224, 224)
+  input <- torch_randn(1, 3, 224, 224)
   model_large$eval()
   out <- model_large(input)
   expect_tensor_shape(out, c(1000))
@@ -30,7 +30,7 @@ test_that("tests for pretrained model_mobilenet_v3_large", {
 
 test_that("tests for non-pretrained model_mobilenet_v3_small", {
   model_small <- model_mobilenet_v3_small(pretrained = FALSE)
-  input <- torch::torch_randn(1, 3, 224, 224)
+  input <- torch_randn(1, 3, 224, 224)
   model_small$eval()
   out <- model_small(input)
   expect_tensor_shape(out, c(1000))
@@ -41,17 +41,61 @@ test_that("tests for non-pretrained model_mobilenet_v3_small", {
 
 test_that("tests for pretrained model_mobilenet_v3_small", {
   model_small <- model_mobilenet_v3_small(pretrained = TRUE)
-  input <- torch::torch_randn(1, 3, 224, 224)
+  input <- torch_randn(1, 3, 224, 224)
   model_small$eval()
   out <- model_small(input)
   expect_tensor_shape(out, c(1000))
 
   model <- model_mobilenet_v3_small(pretrained = FALSE,num_classes = 10)
-  input <- torch::torch_randn(1, 3, 224, 224)
+  input <- torch_randn(1, 3, 224, 224)
   out <- model(input)
   expect_tensor_shape(out, c(10))
 
   rm(model_small)
+  rm(model)
+  gc()
+})
+
+test_that("tests for model_mobilenet_v3_large with non-divisible input shapes", {
+
+  model <- model_mobilenet_v3_large(pretrained = FALSE)
+  input <- torch_randn(1, 3, 223, 225)
+  model$eval()
+  out <- model(input)
+  expect_tensor_shape(out, c(1000))
+  rm(model)
+  gc()
+})
+
+test_that("tests for model_mobilenet_v3_small with non-divisible input shapes", {
+
+  model <- model_mobilenet_v3_small(pretrained = FALSE)
+  input <- torch_randn(1, 3, 223, 225)
+  model$eval()
+  out <- model(input)
+  expect_tensor_shape(out, c(1000))
+  rm(model)
+  gc()
+})
+
+test_that("tests for model_mobilenet_v3_small with varied width_mult", {
+
+  model <- model_mobilenet_v3_small(pretrained = FALSE, width_mult = 0.5)
+  input <- torch_randn(1, 3, 224, 224)
+  model$eval()
+  out <- model(input)
+  expect_tensor_shape(out, c(1000))
+  rm(model)
+  gc()
+})
+
+test_that("tests for model_mobilenet_v3_large with varied width_mult", {
+
+  model <- model_mobilenet_v3_large(pretrained = FALSE, width_mult = 0.5)
+  input <- torch_randn(1, 3, 224, 224)
+  model$eval()
+  out <- model(input)
+  expect_tensor_shape(out, c(1000))
   rm(model)
   gc()
 })
