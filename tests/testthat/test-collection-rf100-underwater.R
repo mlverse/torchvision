c86081e37407a47e0a9d374eaafe0d84e419a15a
C. Regouby <christophe.regouby@free.fr>
2025-09-06 20:13:04 +0200

    fix values and tests for `underwater`

--- tests/testthat/test-collection-rf100-underwater.R
+++ tests/testthat/test-collection-rf100-underwater.R
@@ -12,7 +12,7 @@ test_that("rf100_underwater_collection handles missing files gracefully", {
   )
 })
 
-datasets <- c("pipes", "aquarium", "objects", "coral")
+datasets <- c("pipes", "objects", "coral")
 
 for (ds_name in datasets) {
   test_that(paste0("rf100_underwater_collection loads ", ds_name, " correctly"), {
@@ -36,3 +36,36 @@ for (ds_name in datasets) {
     expect_s3_class(item, "image_with_bounding_box")
   })
 }
+
+test_that(paste0("rf100_underwater_collection loads `aquarium` correctly"), {
+  ds <- rf100_underwater_collection(dataset = "aquarium", split = "train", download = TRUE)
+
+  expect_s3_class(ds, "rf100_underwater_collection")
+  expect_gt(ds$.length(), 1)
+  expect_type(ds$classes, "character")
+  expect_gt(length(unique(ds$classes)), 1)
+
+  item <- ds[1]
+
+  expect_type(item$y, "list")
+  expect_named(item$y, c("labels", "boxes"))
+  expect_type(item$y$labels, "integer")
+  expect_tensor(item$y$boxes)
+  expect_equal(item$y$boxes$ndim, 2)
+  expect_equal(item$y$boxes$size(2), 4)
+  expect_s3_class(item, "image_with_bounding_box")
+
+  ds <- rf100_underwater_collection(dataset = "aquarium", split = "test", download = TRUE)
+
+  expect_s3_class(ds, "rf100_underwater_collection")
+  expect_gt(ds$.length(), 1)
+  expect_type(ds$classes, "character")
+  expect_gt(length(unique(ds$classes)), 1)
+
+  ds <- rf100_underwater_collection(dataset = "aquarium", split = "val", download = TRUE)
+
+  expect_s3_class(ds, "rf100_underwater_collection")
+  expect_gt(ds$.length(), 1)
+  expect_type(ds$classes, "character")
+  expect_gt(length(unique(ds$classes)), 1)
+})
