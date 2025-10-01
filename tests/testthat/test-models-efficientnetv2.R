93115ec895f38cd2fcef4314b30fed392247ad39
cregouby <cregouby@users.noreply.github.com>
2025-10-01 21:58:23 +0200

    exclude 11 large models from tests (#261)

--- tests/testthat/test-models-efficientnetv2.R
+++ tests/testthat/test-models-efficientnetv2.R
@@ -1,12 +1,33 @@
+test_that("efficientnet v2 small model produce correct output shapes", {
+
+  # without pretrained
+  model <- model_efficientnet_v2_s(pretrained = FALSE)
+  input <- torch::torch_randn(1, 3, 384, 384)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 1000))
+
+  # with pretrained
+  withr::with_options(list(timeout = 360), {
+    model <- model_efficientnet_v2_s(pretrained = TRUE)
+  })
+  input <- torch::torch_randn(1, 3, 384, 384)
+  out <- model(input)
+  expect_tensor_shape(out, c(1, 1000))
+
+  unlink_model_file()
+})
+
+
 test_that("efficientnet v2 models produce correct output shapes", {
+  skip_if(Sys.getenv("TEST_LARGE_DATASETS", unset = "0") != "1",
+          "Skipping test: set TEST_LARGE_DATASETS=1 to enable tests requiring large downloads.")
 
   variants <- list(
-    s = model_efficientnet_v2_s,
     m = model_efficientnet_v2_m,
     l = model_efficientnet_v2_l
   )
 
-  sizes <- c(s = 384, m = 480, l = 512)
+  sizes <- c(m = 480, l = 512)
 
   for (name in names(variants)) {
     fn <- variants[[name]]
