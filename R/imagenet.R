2afc7479f6b6209a8f91034ba896a3fc50b9fe77
C. Regouby <christophe.regouby@free.fr>
2026-02-04 00:19:55 +0100

    add imagenet_21k

--- R/imagenet.R
+++ R/imagenet.R
@@ -23,3 +23,30 @@ imagenet_label <- function(id) {
   classes <- imagenet_classes()
   classes[id]
 }
+
+imagenet_1k_classes <- imagenet_classes
+imagenet_1k_label <- imagenet_label
+
+#' @return A character vector with 21k entries representing the ImageNet-21k
+#'   class labels.
+#' @family class_resolution
+#' @export
+imagenet_21k_classes <- function() {
+  url <- "https://storage.googleapis.com/bit_models/imagenet21k_wordnet_ids.txt"
+  ids <- readLines(url, warn = FALSE)
+  url <- "https://storage.googleapis.com/bit_models/imagenet21k_wordnet_lemmas.txt"
+  labels <- readLines(url, warn = FALSE)
+
+  data.frame(id = ids, label = labels)
+}
+
+#' @rdname imagenet_classes
+#' @param id Integer vector of 1-based class identifiers.
+#' @return A character vector with the labels associated with `id`.
+#' @family class_resolution
+#' @export
+imagenet_21k_label <- function(id) {
+  classes <- imagenet_21k_classes()$label
+  classes[id]
+}
+
