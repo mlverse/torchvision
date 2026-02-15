15f70d0c2bff0dd35bb52cf4c7cd46461c920a5b
C. Regouby <christophe.regouby@free.fr>
2026-02-15 18:48:49 +0100

    strongly reduce test time

--- tests/testthat/test-models-faster_rcnn.R
+++ tests/testthat/test-models-faster_rcnn.R
@@ -1,6 +1,6 @@
 test_that("tests for non-pretrained model_fasterrcnn_resnet50_fpn", {
 
-  model <- model_fasterrcnn_resnet50_fpn()
+  model <- model_fasterrcnn_resnet50_fpn(score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/cat/cat.0.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(200,200)) %>% torch_unsqueeze(1)
   model$eval()
@@ -13,7 +13,7 @@ test_that("tests for non-pretrained model_fasterrcnn_resnet50_fpn", {
   expect_equal(out$detections$boxes$shape[2], 4L)
 
 
-  model <- model_fasterrcnn_resnet50_fpn(num_classes = 10)
+  model <- model_fasterrcnn_resnet50_fpn(num_classes = 10, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   out <- model(input)
   expect_named(out, c("features","detections"))
   expect_named(out$detections, c("boxes","labels", "scores"))
@@ -27,7 +27,7 @@ test_that("tests for non-pretrained model_fasterrcnn_resnet50_fpn_v2", {
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_resnet50_fpn_v2()
+  model <- model_fasterrcnn_resnet50_fpn_v2(score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/cat/cat.1.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   model$eval()
@@ -40,7 +40,7 @@ test_that("tests for non-pretrained model_fasterrcnn_resnet50_fpn_v2", {
   expect_equal(out$detections$boxes$shape[2], 4L)
 
 
-  model <- model_fasterrcnn_resnet50_fpn_v2(num_classes = 10)
+  model <- model_fasterrcnn_resnet50_fpn_v2(num_classes = 10, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   out <- model(input)
   expect_named(out, c("features","detections"))
   expect_named(out$detections, c("boxes","labels", "scores"))
@@ -54,7 +54,7 @@ test_that("tests for non-pretrained model_fasterrcnn_mobilenet_v3_large_fpn", {
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_mobilenet_v3_large_fpn()
+  model <- model_fasterrcnn_mobilenet_v3_large_fpn(score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/cat/cat.2.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   model$eval()
@@ -67,7 +67,7 @@ test_that("tests for non-pretrained model_fasterrcnn_mobilenet_v3_large_fpn", {
   expect_equal(out$detections$boxes$shape[2], 4L)
 
 
-  model <- model_fasterrcnn_resnet50_fpn_v2(num_classes = 10)
+  model <- model_fasterrcnn_resnet50_fpn_v2(num_classes = 10, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   out <- model(input)
   expect_named(out, c("features","detections"))
   expect_named(out$detections, c("boxes","labels", "scores"))
@@ -81,7 +81,7 @@ test_that("tests for non-pretrained model_fasterrcnn_mobilenet_v3_large_320_fpn"
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_mobilenet_v3_large_320_fpn()
+  model <- model_fasterrcnn_mobilenet_v3_large_320_fpn(score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/cat/cat.3.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   model$eval()
@@ -94,7 +94,7 @@ test_that("tests for non-pretrained model_fasterrcnn_mobilenet_v3_large_320_fpn"
   expect_equal(out$detections$boxes$shape[2], 4L)
 
 
-  model <- model_fasterrcnn_resnet50_fpn_v2(num_classes = 10)
+  model <- model_fasterrcnn_resnet50_fpn_v2(num_classes = 10, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   out <- model(input)
   expect_named(out, c("features","detections"))
   expect_named(out$detections, c("boxes","labels", "scores"))
@@ -108,7 +108,7 @@ test_that("tests for pretrained model_fasterrcnn_resnet50_fpn", {
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_resnet50_fpn(pretrained = TRUE)
+  model <- model_fasterrcnn_resnet50_fpn(pretrained = TRUE, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/cat/cat.4.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   out <- model(input)
@@ -124,7 +124,7 @@ test_that("tests for pretrained model_fasterrcnn_resnet50_fpn_v2", {
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_resnet50_fpn_v2(pretrained = TRUE)
+  model <- model_fasterrcnn_resnet50_fpn_v2(pretrained = TRUE, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/cat/cat.5.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   out <- model(input)
@@ -140,7 +140,7 @@ test_that("tests for pretrained model_fasterrcnn_mobilenet_v3_large_fpn", {
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_mobilenet_v3_large_fpn(pretrained = TRUE)
+  model <- model_fasterrcnn_mobilenet_v3_large_fpn(pretrained = TRUE, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/dog/dog.0.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   out <- model(input)
@@ -156,7 +156,7 @@ test_that("tests for pretrained model_fasterrcnn_mobilenet_v3_large_320_fpn", {
   skip_if(Sys.getenv("TEST_LARGE_MODELS", unset = 0) != 1,
           "Skipping test: set TEST_LARGE_MODELS=1 to enable tests requiring large downloads.")
 
-  model <- model_fasterrcnn_mobilenet_v3_large_320_fpn(pretrained = TRUE)
+  model <- model_fasterrcnn_mobilenet_v3_large_320_fpn(pretrained = TRUE, score_thresh = 0.6, nms_thresh = 0.9, detections_per_img = 100)
   input <- base_loader("assets/class/dog/dog.1.jpg") %>%
     transform_to_tensor() %>% transform_resize(c(180,180)) %>% torch_unsqueeze(1)
   out <- model(input)
