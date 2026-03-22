aacf79ed79f4871d921885366a1dc42a65a2b25c
DerrickUnleashed <derricksamuel.r@gmail.com>
2026-03-23 00:56:40 +0530

    Adding UI to the keypoints.R example

--- vignettes/examples/keypoints.R
+++ vignettes/examples/keypoints.R
@@ -1,21 +1,22 @@
-# Load image using base_loader
+# Load image using base_loader ---------------------------------------------------
 url <- "https://raw.githubusercontent.com/pytorch/vision/main/gallery/assets/dog1.jpg"
 image <- base_loader(url) %>% transform_to_tensor()
 
-# 1 triangle, 3 keypoints each (x, y)
+# 1 triangle, 3 keypoints each (x, y) --------------------------------------------
 keypoints <- torch_tensor(rbind(
   c(263, 225),
   c(238, 220),
   c(242, 247)
 ), dtype = torch_float())$reshape(c(1L, 3L, 2L))
 
-# connectivity to form a triangle
+# connectivity to form a triangle ------------------------------------------------
 connectivity <- list(
   c(1L, 2L),
   c(1L, 3L),
   c(2L, 3L)
 )
 
+# Visualize keypoints ------------------------------------------------------------
 triangle_img <- draw_keypoints(
   image,
   keypoints,
