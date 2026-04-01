b17fdaa0a870d081dbb24934d5ff5c0eb9513404
to94498 <christophe.regouby@airbus.com>
2026-04-01 17:50:26 +0200

    add .list tests

--- tests/testthat/test-transforms-array.R
+++ tests/testthat/test-transforms-array.R
@@ -29,3 +29,29 @@ test_that("transform_to_tensor works for 2D, 3D, 4D array", {
   expect_tensor_dtype(o, torch_float())
 
 })
+
+test_that("transform_to_tensor works for list of arrays", {
+  x <- array(1L:120L, dim = c(10,12))
+  o <- transform_to_tensor(list(x, x))
+  expect_tensor_shape(o, c(2,1,10,12))
+  expect_tensor_dtype(o, torch_float())
+  expect_equal_to_r(o$max(), 120/255)
+
+  x <- array(1L:120L, dim = c(1, 10,12))
+  o <- transform_to_tensor(list(x, x))
+  expect_tensor_shape(o, c(2,1,10,12))
+  expect_tensor_dtype(o, torch_float())
+  expect_equal_to_r(o$max(), 120/255)
+
+  x <- array(1L:120L, dim = c(3, 5, 8))
+  o <- transform_to_tensor(list(x, x, x))
+  expect_tensor_shape(o, c(3, 3, 5, 8))
+  expect_tensor_dtype(o, torch_float())
+  expect_equal_to_r(o$max(), 120/255)
+
+
+  x <- array(1L:120L, dim = c(2, 1, 4,5))
+  expect_error(transform_to_tensor(list(x,x)),
+               "3D arrays")
+
+})
