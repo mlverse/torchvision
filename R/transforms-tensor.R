b3623726692c7e324e0cffa909600f2447cb7cbe
to94498 <christophe.regouby@airbus.com>
2026-04-08 20:20:27 +0200

    fix linear_transformation and adjust_hue

--- R/transforms-tensor.R
+++ R/transforms-tensor.R
@@ -243,8 +243,8 @@ transform_five_crop.torch_tensor <- function(img, size) {
   if (!length(size) == 2)
     value_error("Please provide only 2 dimensions (h, w) for size.")
 
-  image_width <- img$size(2)
-  image_height <- img$size(3)
+  image_width <- img$size(-2)
+  image_height <- img$size(-1)
 
   crop_height <- size[1]
   crop_width <- size[2]
@@ -284,7 +284,8 @@ transform_ten_crop.torch_tensor <- function(img, size, vertical_flip = FALSE) {
 #' @export
 transform_linear_transformation.torch_tensor <- function(img, transformation_matrix,
                                                          mean_vector) {
-  flat_tensor <- img$view(c(1, -1)) - mean_vector
+  flat_dim <- if (img$ndim == 3) c(1, -1) else c(img$size(1), -1)
+  flat_tensor <- img$view(flat_dim) - mean_vector
   transformed_tensor <- torch::torch_mm(flat_tensor, transformation_matrix)
 
   transformed_tensor$view(img$size())
@@ -366,10 +367,12 @@ transform_adjust_hue.torch_tensor <- function(img, hue_factor) {
     img <- img$to(dtype = torch::torch_float32())/255
 
   img <-rgb2hsv(img)
-  hsv <- img$unbind(1)
+
+  channel_dim <- if (img$ndim == 3) 1 else 2
+  hsv <- img$unbind(channel_dim)
   hsv[[1]] <- hsv[[1]] + hue_factor
   hsv[[1]] <- hsv[[1]] %% 1
-  img <- torch::torch_stack(hsv)
+  img <- torch::torch_stack(hsv, dim=channel_dim)
   img_hue_adj <- hsv2rgb(img)
 
   if (orig_dtype == torch::torch_uint8())
@@ -475,12 +478,13 @@ blend <- function(img1, img2, ratio) {
 }
 
 rgb2hsv <- function(img) {
+  channel_dim <- if (img$ndim == 3) 1 else 2
 
-  rgb <- img$unbind(1)
+  rgb <- img$unbind(channel_dim)
   r <- rgb[[1]]; g <- rgb[[2]]; b <- rgb[[3]]
 
-  maxc <- torch::torch_max(img, dim=1)[[1]]
-  minc <- torch::torch_min(img, dim=1)[[1]]
+  maxc <- torch::torch_max(img, dim=channel_dim)[[1]]
+  minc <- torch::torch_min(img, dim=channel_dim)[[1]]
 
   # The algorithm erases S and H channel where `maxc = minc`. This avoids NaN
   # from happening in the results, because
@@ -510,12 +514,14 @@ rgb2hsv <- function(img) {
   hb <- ((maxc != g) & (maxc != r)) * (4.0 + gc - rc)
   h <- (hr + hg + hb)
   h <- torch::torch_fmod((h / 6.0 + 1.0), 1.0)
-  torch::torch_stack(list(h, s, maxc))
+  torch::torch_stack(list(h, s, maxc), dim=channel_dim)
 }
 
 hsv2rgb <- function(img) {
 
-  hsv <- img$unbind(1)
+  channel_dim <- if (img$ndim == 3) 1 else 2
+
+  hsv <- img$unbind(channel_dim)
   i <- torch::torch_floor(hsv[[1]] * 6)
   f <- (hsv[[1]] * 6) - 1
   i <- i$to(dtype = torch::torch_int32())
@@ -525,14 +531,20 @@ hsv2rgb <- function(img) {
   t <- torch::torch_clamp((hsv[[3]] * (1 - f)), 0, 1)
   i <- i %% 6
 
-  mask <- i == torch::torch_arange(start= 0, end = 5)[,NULL,NULL]
+  mask <- if (img$ndim == 3)
+    i == torch::torch_arange(start= 0, end = 5)[,NULL,NULL]
+  else
+    i == torch::torch_arange(start= 0, end = 5)[,NULL,NULL,NULL]
 
   a1 <- torch::torch_stack(list(hsv[[3]], q, p, p, t, hsv[[3]]))
   a2 <- torch::torch_stack(list(t, hsv[[3]], hsv[[3]], q, p, p))
   a3 <- torch::torch_stack(list(p, p, t, hsv[[3]], hsv[[3]], q))
   a4 <- torch::torch_stack(list(a1, a2, a3))
 
-  torch::torch_einsum("ijk, xijk -> xjk", list(mask$to(dtype = img$dtype), a4))
+  if (img$ndim == 3)
+    torch::torch_einsum("ijk, xijk -> xjk", list(mask$to(dtype = img$dtype), a4))
+  else
+    torch::torch_einsum("ibjk, xibjk -> bxjk", list(mask$to(dtype = img$dtype), a4))
 }
 
 # https://stackoverflow.com/questions/32370485/convert-radians-to-degree-degree-to-radians
