9762aa90467737bb99454122e4086550ac4ff56d
C. Regouby <christophe.regouby@free.fr>
2026-04-09 18:50:25 +0200

    transform_to_tensor.array manage 4D tensors, simplify mnist_dataset$.getbatch

---
+++ tests/testthat/test-transforms-array.R
@@ -0,0 +1,31 @@
+test_that("transform_to_tensor works for 2D, 3D, 4D array", {
+  x <- array(1L:120L, dim = c(10,12))
+  o <- transform_to_tensor(x)
+  expect_tensor_shape(o, c(1,10,12))
+  expect_tensor_dtype(o, torch_float())
+  expect_equal_to_r(o$max(), 120/255)
+
+  x <- array(1L:120L, dim = c(1, 10,12))
+  o <- transform_to_tensor(x)
+  expect_tensor_shape(o, c(1,10,12))
+  expect_tensor_dtype(o, torch_float())
+  expect_equal_to_r(o$max(), 120/255)
+
+  x <- array(1L:120L, dim = c(3, 5, 8))
+  o <- transform_to_tensor(x)
+  expect_tensor_shape(o, c(3, 5, 8))
+  expect_tensor_dtype(o, torch_float())
+  expect_equal_to_r(o$max(), 120/255)
+
+
+  x <- array(1L:120L, dim = c(2, 1, 4,5))
+  o <- transform_to_tensor(x)
+  expect_tensor_shape(o, c(2, 1, 4, 5))
+  expect_tensor_dtype(o, torch_float())
+
+  x <- array(1L:120L, dim = c(2, 3, 10,12))
+  o <- transform_to_tensor(x)
+  expect_tensor_shape(o, c(2, 3,10,12))
+  expect_tensor_dtype(o, torch_float())
+
+})
