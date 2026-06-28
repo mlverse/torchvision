265a59c4efa18356e3c15c484d514d1dbc514cbf
DerrickUnleashed <derricksamuel.r@gmail.com>
2026-06-28 21:48:52 +0530

    Remove convert-then-split test case

--- tests/testthat/test-transforms-array.R
+++ tests/testthat/test-transforms-array.R
@@ -56,21 +56,6 @@ test_that("transform_to_tensor works for list of arrays", {
 
 })
 
-test_that("transform_sahi_crop works with arrays via convert-then-split", {
-
-  arr <- array(sample(0:255, 3 * 80 * 60, replace = TRUE), dim = c(80, 60, 3))
-
-  x <- transform_to_tensor(arr)
-  sp <- prepare_sahi_split(x, size = c(40, 30), overlap_size_ratio = c(0.5, 0.5))
-  res <- transform_sahi_crop(x, sp)
-
-  expect_tensor(res)
-  expect_equal(res$ndim, 4)
-  expect_gt(res$size(1), 0)
-  expect_equal(res$size(2), 3)
-
-})
-
 test_that("transform_sahi_crop works with batched 4D array input", {
 
   arr <- array(sample(0:255, 2 * 3 * 80 * 60, replace = TRUE), dim = c(2, 3, 80, 60))
