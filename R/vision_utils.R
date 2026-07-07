88570f04c63ac38ad70b0c25fd1fa960028335df
cregouby <christophe.regouby@free.Fr>
2026-07-08 00:14:32 +0200

    add NEWS

--- R/vision_utils.R
+++ R/vision_utils.R
@@ -308,7 +308,7 @@ coco_polygon_to_mask <- function(segmentation, height, width) {
 #'
 #' @examplesIf torch::torch_is_installed() && rlang::is_installed("magick")
 #' image_tensor <- torch::torch_randint(170, 250, size = c(3, 360, 360))$to(torch::torch_uint8())
-#' mask <- torch::torch_tril(torch::torch_ones(c(360, 360)))$to(torch_bool())
+#' mask <- torch::torch_tril(torch::torch_ones(c(360, 360)))$to(torch::torch_bool())
 #' masked_image <- draw_segmentation_masks(image_tensor, mask, alpha = 0.2)
 #' tensor_image_browse(masked_image)
 #' @family image display
