5f8a2a1ffccac6ec905d9fa7d7a0617a7e25f283
DerrickUnleashed <derricksamuel.r@gmail.com>
2026-08-12 21:47:50 +0530

    fix: delete fer2013 archive after extraction

--- R/dataset-fer.R
+++ R/dataset-fer.R
@@ -130,6 +130,7 @@ fer_dataset <- dataset(
     }
 
     untar(archive, exdir = self$root)
+    fs::file_delete(archive)
     cli_inform(
       "Dataset {.cls {class(self)[[1]]}} downloaded and extracted successfully."
     )
